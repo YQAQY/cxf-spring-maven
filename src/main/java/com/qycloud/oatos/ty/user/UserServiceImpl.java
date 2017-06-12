d242e0e937d83009d9f9096dd6c82ef7f97f7d3a
kingflag <1362168862@qq.com>
2017-06-12 09:06:16 +0800

    cxf3.x +spring 3.x（4.x）+ maven 发布webservice 服务

---
+++ src/main/java/com/qycloud/oatos/ty/user/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.qycloud.oatos.ty.user;
+
+import javax.jws.WebService;
+
+
+@WebService(endpointInterface = "com.qycloud.oatos.ty.user.UserService")
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public boolean updateTheUser(String user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateUsersCode(String user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
